88a5990dfca7201233b6e710d45daf4b2b35af6e
Gibbyswag <will.gibson7@gmail.com>
2020-04-24 18:16:28 -0700

    changed db connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 var connection;
 
-// if (process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
   connection = mysql.createConnection({
       host: "localhost",
       port: 3306,
@@ -13,7 +13,7 @@ var connection;
       password: "password",
       database: "burgers_db"
   });
-// };
+};
 
 connection.connect(function (err) {
   if (err) {
